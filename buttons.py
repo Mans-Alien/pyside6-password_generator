9895d9a3ffbe12df2e85e9f2793bee2c8022f64f
Mans-Alien <mans012033945420@gmail.com>
2023-08-22 14:51:34 +0300

    simple password generator

---
+++ buttons.py
@@ -0,0 +1,8 @@
+from string import ascii_lowercase, ascii_uppercase, digits, punctuation
+from enum import Enum
+
+class Characters(Enum):
+    btn_lower = ascii_lowercase
+    btn_upper = ascii_uppercase
+    btn_digits = digits
+    btn_simbols = punctuation
---
+++ password.py
@@ -0,0 +1,4 @@
+import secrets
+
+def create_new (length: int, characters: str) -> str:
+    return "".join(secrets.choice(characters) for _ in range(length))
